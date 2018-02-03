e1fc8233ad24af42c913f36895f801c15bf47e81
Nik Macintosh <nik@eventbase.com>
2018-02-02 23:03:43 -0800

    Split `UINotificationCenter.addObserver()` calls over multiple lines to come in under line_length 120

--- MessageViewController/MessageViewController.swift
+++ MessageViewController/MessageViewController.swift
@@ -89,11 +89,36 @@ open class MessageViewController: UIViewController, MessageAutocompleteControlle
         messageAutocompleteController.layoutDelegate = self
 
         let notificationCenter = NotificationCenter.default
-        notificationCenter.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyboardDidShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(keyboardDidHide(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
-        notificationCenter.addObserver(self, selector: #selector(appWillResignActive(notification:)), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(notification:)),
+            name: NSNotification.Name.UIKeyboardWillShow,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardDidShow(notification:)),
+            name: NSNotification.Name.UIKeyboardDidShow,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(notification:)),
+            name: NSNotification.Name.UIKeyboardWillHide,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardDidHide(notification:)),
+            name: NSNotification.Name.UIKeyboardDidHide,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(appWillResignActive(notification:)),
+            name: NSNotification.Name.UIApplicationWillResignActive,
+            object: nil
+        )
     }
 
     internal var safeAreaAdditionalHeight: CGFloat {
