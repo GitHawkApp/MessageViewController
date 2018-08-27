7a96a98d10c2abd233fd9e6ee0cd5d9f19c38162
Jason Ibrahim <jasonaibrahim@gmail.com>
2018-08-27 13:35:04 -0700

    remove needsLayout as its causing the scroll view to jump

--- MessageViewController/MessageViewController.swift
+++ MessageViewController/MessageViewController.swift
@@ -233,7 +233,6 @@ open class MessageViewController: UIViewController, MessageAutocompleteControlle
     // MARK: MessageAutocompleteControllerLayoutDelegate
 
     public func needsLayout(controller: MessageAutocompleteController) {
-        view.setNeedsLayout()
     }
 
 }
